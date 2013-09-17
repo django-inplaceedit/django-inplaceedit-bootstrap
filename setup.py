a13ff87d418ae88682a24b802cb2a387cbde1bbd
Pablo Martín <goinnn@gmail.com>
2013-09-17 12:45:00 +0200

    Update the django-inplaceedit

--- setup.py
+++ setup.py
@@ -41,5 +41,5 @@ setup(
     packages=find_packages('.'),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['django-inplaceedit==1.2.2']
+    install_requires=['django-inplaceedit>=1.2.3']
 )
